fb40731447e1d55ed2f776423dc7d4868c518836
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-22 09:50:36 -0400

    Add .env

--- bookcollection/settings.py
+++ bookcollection/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8dg8w%zy#-a7m*3_3^dmr76wm66u9*ozsw%x#sqf4k7oxwba=$'
+SECRET_KEY= env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
